5e3c371dd9ba06bf24a2d45a43f7c128a5a90611
Olivier Precourt <olivierprecourt@live.ca>
2015-01-30 09:51:59 -0500

    Skelette fait
    les méthodes ont été créées mais sont vides

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -1,1 +1,25 @@
 #include "dynamicArray.h"
+
+DynamicArray::DynamicArray(){
+
+}
+
+DynamicArray::DynamicArray(unsigned int _capacite){
+
+}
+
+void DynamicArray::setElement(unsigned int _index, int valeur){
+
+}
+
+int DynamicArray::getElement(unsigned int _index){
+
+}
+
+unsigned int DynamicArray::getCapacite(){
+
+}
+
+void DynamicArray::setCapacite(unsigned int _capacite){
+
+}
--- DynamicArray/dynamicArray.h
+++ DynamicArray/dynamicArray.h
@@ -6,12 +6,16 @@ class DynamicArray
 {
 
 public:
-
-
-
+	DynamicArray();
+	DynamicArray(unsigned int _capacite);
+	~DynamicArray();
+	void setElement(unsigned int _index, int _valeur);
+	int getElement(unsigned int index);
+	unsigned int getCapacite();
+	void setCapacite(unsigned int _capacite);
 private:
-
-
+	int* tabElement;
+	unsigned int capacite;
 };
 
 
