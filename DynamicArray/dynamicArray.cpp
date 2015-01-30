9fdbf99f941dd4ab1fd613d348a039c7d51a5489
Olivier Precourt <olivierprecourt@live.ca>
2015-01-30 10:31:20 -0500

    Exceptions fonctionnelles

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -1,7 +1,11 @@
 #include "dynamicArray.h"
 #include <stdexcept>
 
+using namespace std;
+
 const int TAILLE_PAR_DEFAUT_TABLEAU = 100;
+const string ERREUR_HORS_DE_INDEX = "l'element n'existe pas";
+const string ERREUR_CAPACITE_INVALIDE = "capacite invalide";
 
 DynamicArray::~DynamicArray(){
 	delete tabElement;
@@ -17,26 +21,34 @@ DynamicArray::DynamicArray(){
 }
 
 DynamicArray::DynamicArray(unsigned int _capacite){
-	tabElement = new int[_capacite];
-	capacite = _capacite;
+	if (_capacite < 1) {
+		throw invalid_argument(ERREUR_CAPACITE_INVALIDE);
+	}
+	else {
+		tabElement = new int[_capacite];
+		capacite = _capacite;
 
-	for (int i = 0; i < _capacite; i++){
-		tabElement[i] = 0;
+		for (int i = 0; i < _capacite; i++){
+			tabElement[i] = 0;
+		}
 	}
 }
 
 void DynamicArray::setElement(unsigned int _index, int _valeur){
+	
 	if (_index >= capacite){
 		//on augmente la capacité
 	}
-	else {
+	else if (_index < 0) {
+		throw invalid_argument(ERREUR_HORS_DE_INDEX);
+	} else {
 		tabElement[_index] = _valeur;
 	}
 }
 
 int DynamicArray::getElement(unsigned int _index) const {
 	if (_index >= capacite){
-		throw  std::runtime_error("l'element n'existe pas");
+		throw  invalid_argument(ERREUR_HORS_DE_INDEX);
 	}
 	else {
 		return tabElement[_index];
@@ -48,6 +60,11 @@ int DynamicArray::getCapacite() const {
 }
 
 void DynamicArray::setCapacite(unsigned int _capacite){
-	capacite = _capacite;
+	if (_capacite < 1) {
+		throw invalid_argument(ERREUR_CAPACITE_INVALIDE);
+	}
+	else {
+		capacite = _capacite;
+	}
 }
 
